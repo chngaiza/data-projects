fbbdfad7db133f549a398de851ec0c9454e7d5a6
chngaiza <98628962+chngaiza@users.noreply.github.com>
2026-03-29 12:20:28 -0400

    Update PostgreSQL Analysis Queries - Student Instruction Database.sql

--- Student Instruction Database & Analysis Project/PostgreSQL Analysis Queries - Student Instruction Database.sql
+++ Student Instruction Database & Analysis Project/PostgreSQL Analysis Queries - Student Instruction Database.sql
@@ -1,23 +1,34 @@
 1.	What’s the relationship between sessions length and essay grades?
 
-SELECT s.student_id, s.session_length_min, er.grade 
-FROM essays AS e 
-INNER JOIN sessions AS s ON e.id = s.essay_id
-INNER JOIN essay_results AS er ON e.id = er.essay_id;
+select 
+  s.student_id, 
+  s.session_length_min, 
+  er.grade 
+from essays e 
+inner join sessions s 
+  on e.id = s.essay_id
+inner join essay_results er 
+  on e.id = er.essay_id;
 
 2.	What courses typically receive the highest grades?
+  
+select 
+  course.id, 
+  course.course_name, 
+  essay_results.grade
+from essays, course, essay_results
+where essays.id = essay_results.essay_id
+and course.id = essays.id
+group by course.id, course.course_name, essay_results.grade
+order by essay_results.grade desc;
 
-SELECT course.id, course.course_name, essay_results.grade
-FROM essays, course, essay_results
-WHERE essays.id = essay_results.essay_id
-AND course.id = essays.id
-GROUP BY course.id, course.course_name, essay_results.grade
-ORDER BY essay_results.grade;
 
-CREATE PROCEDURE top_course_grades AS 
-SELECT course.id, course.course_name, essay_results.grade
-FROM essays, course, essay_results
-WHERE essays.id = essay_results.essay_id
-AND course.id = essays.id
-GROUP BY course.id, course.course_name, essay_results.grade
-ORDER BY essay_results.grade;
+select 
+  course.id, 
+  course.course_name, 
+  essay_results.grade
+from essays, course, essay_results
+where essays.id = essay_results.essay_id
+and course.id = essays.id
+group by course.id, course.course_name, essay_results.grade
+order BY essay_results.grade;
